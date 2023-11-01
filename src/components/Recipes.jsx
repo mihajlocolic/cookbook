0fca5ce0a67bb653606a005e56120bbdf543c52a
Mihajlo Colic <colicmihajlo@gmail.com>
2023-11-01 14:44:30 +0100

    List of recipes is now reversed to show the latest ones first

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,7 +3,7 @@ const Recipes = ({recipes}) => {
 
     return (
         <div className="recipe-list">
-            { recipes && recipes.map(recipe => (
+            { recipes && recipes.toReversed().map(recipe => (
                 <div className="card" style={{width: 25 + 'rem'}} key={recipe.id}>
                     <Link to={`/recipes/${recipe.id}`}>
                     <img src={recipe.image} className="card-img-top" alt="..."/>
